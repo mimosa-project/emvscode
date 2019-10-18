aef9a8ae923701a5b75a90ad13772b78342a9d0e
fpsbpkm <taisei4510471a@yahoo.co.jp>
2019-10-18 11:47:15 +0900

    インデントの修正

--- src/extension.ts
+++ src/extension.ts
@@ -3,30 +3,30 @@ import { mizar_it } from './mizarFunctions';
 
 export function activate(context: vscode.ExtensionContext) {
 
-	//verifierの実行結果を出力するチャンネル
-	var channel = vscode.window.createOutputChannel('output');
+    //verifierの実行結果を出力するチャンネル
+    var channel = vscode.window.createOutputChannel('output');
     channel.show();
 
-	let disposable1 = vscode.commands.registerCommand('extension.helloWorld', () => {
-		vscode.window.showInformationMessage('Hello World!');
-	});
+    let disposable1 = vscode.commands.registerCommand('extension.helloWorld', () => {
+        vscode.window.showInformationMessage('Hello World!');
+    });
 
-	let disposable2 = vscode.commands.registerCommand('mizar-it', () => {
+    let disposable2 = vscode.commands.registerCommand('mizar-it', () => {
         //アクティブなエディタがなければエラーを示して終了
         if (vscode.window.activeTextEditor === undefined){
             vscode.window.showErrorMessage('Not currently in .miz file!!');
             return;
-		}
-		
-		//アクティブなファイルのパスを取得
-		let fileName = vscode.window.activeTextEditor.document.fileName;
+        }
 
-		//makeenvとverifierの実行
-		mizar_it(channel,fileName);
-	});
+        //アクティブなファイルのパスを取得
+        let fileName = vscode.window.activeTextEditor.document.fileName;
 
-	context.subscriptions.push(disposable1);
-	context.subscriptions.push(disposable2);
+        //makeenvとverifierの実行
+        mizar_it(channel,fileName);
+    });
+
+    context.subscriptions.push(disposable1);
+    context.subscriptions.push(disposable2);
 }
 
 // this method is called when your extension is deactivated
