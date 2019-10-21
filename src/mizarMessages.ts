64bd424cfa2ab3645f3868f7c58055c421bbfb8a
TH <32231297+fpsbpkm@users.noreply.github.com>
2019-10-21 16:16:28 +0900

    エラーコード->エラーメッセージ の処理を行う関数の追加 (#14)
    * エラーコード -> エラーメッセージの処理を行う関数のファイルを追加
    
    * エラーコード->エラーメッセージを行う関数の読み込み
    
    * エラーを表示する関数displayProblems,displayErrorLinksの追加
    
    * 誤ったコミットの取り消し
    
    * ステートマシンとして修正
    不要な処理、行の削除

---
+++ src/mizarMessages.ts
@@ -0,0 +1,39 @@
+import * as fs from 'fs';
+import { mizfiles } from './mizarFunctions';
+import * as path from 'path';
+
+export function makeQueryFunction() {
+    let errNo2Msg:{ [n: number]: string } = {};
+    function _queryMizarMsg(_errNo:number) {
+        if (!Object.keys(errNo2Msg).length) {
+            let key:number = 0;
+            let isReadingErrorMsg:boolean = false;
+            let MizarMessage = "" + fs.readFileSync(path.join(String(mizfiles),'mizar.msg'));
+            let MizarMessageList = MizarMessage.split('\r\n'||'\n');
+
+            // mizar.msgの中身(一部)
+            // # 243
+            // This correctness condition is not allowed in a redefinition with assumptions
+            // # 250
+            // "$1",...,"$10" are only allowed inside the definiens of a private constructor
+            // # 251
+            // "it" is only allowed inside the definiens of a public functor or mode
+                     
+            for (let line of MizarMessageList) {
+                // 読み込んだ1行から「# 数字」を抽出する
+                let match = line.match(/# (\d+)/);
+                if (match){
+                    // 「# 数字」の次の行はエラーメッセージであるため、trueを設定
+                    isReadingErrorMsg = true;
+                    key = Number(match[1]);
+                }
+                else if(isReadingErrorMsg){
+                    errNo2Msg[key] = line;
+                    isReadingErrorMsg = false;
+                }
+            }
+        }
+        return errNo2Msg[_errNo];
+    }
+    return _queryMizarMsg;
+}
