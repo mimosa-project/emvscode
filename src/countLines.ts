45b96777b9d9f3cbc974eecab88d46e991d61308
fpsbpkm <taisei4510471a@yahoo.co.jp>
2019-11-20 17:11:33 +0900

    環境部と記述部の行数を取得する処理を1つの関数に実装

--- src/countLines.ts
+++ src/countLines.ts
@@ -1,48 +1,39 @@
 import * as fs from "fs";
+import { env } from "vscode";
 
 /**
  * @fn
- * mizarファイルの記述部の行数を返す関数
+ * mizarファイルの環境部・記述部の行数を返す関数
  * スペースや改行だけの行は、それ以降に他の記述がなければカウントされない
  * @param (fileName) 記述部の行数を取得する対象のファイル名
- * @return 記述部の行数
+ * @return 環境部・記述部の行数のリスト
  */
-export function countArticleLines(fileName:string):number{
-    let count:number = 0;
-    let result:number = 0;
+export function countLines(fileName:string):number[]{
+    let articleCounter:number = 0;
+    let environmentalCounter:number = 0;
+    let result:number[] = [];
     let file = "" + fs.readFileSync(fileName);
-    let fileList = file.split('\r\n'||'\n');
+    let lines = file.split('\r\n'||'\n');
     let isArticleArea = false;
-    for (let line of fileList){
+    for (let line of lines){
+        // begin以降から記述部が始まるためisArticleAreaにtrueを設定
         if(line.indexOf('begin') !== -1){
             isArticleArea = true;
         }
+        // 記述部の行数のカウント
         if(isArticleArea){
-            count += 1;
+            articleCounter++;
         }
+        // 環境部の行数のカウント
+        else{
+            environmentalCounter++;
+        }
+        // 改行やスペース以外の記述がある行数までを結果に格納する
+        // つまりアルファベットや数字が記述されている行で最も大きい行数が返され、
+        // それ以降の改行やスペースのみの行はカウントされないことになる
         if(/\w+/.test(line)){
-            result = count;
+            result = [environmentalCounter,articleCounter];
         }
     }
     return result;
-}
-
-/**
- * @fn
- * 環境部の行数を返す関数
- * beginが記述される直前の行数までを返す
- * @param (fileName) 環境部の行数を取得する対象のファイル名
- * @return 環境部の行数
- */
-export function countEnvironmentalLines(fileName:string):number{
-    let count:number = 0;
-    let file = "" + fs.readFileSync(fileName);
-    let fileList = file.split('\r\n'||'\n');
-    for (let line of fileList){
-        if(line.indexOf('begin') !== -1){
-            break;
-        }
-        count++;
-    }
-    return count;
 }
