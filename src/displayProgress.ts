40dbd5dfcc199826fc20a69cf63ce18895c05b4e
fpsbpkm <taisei4510471a@yahoo.co.jp>
2019-11-20 17:12:49 +0900

    変数名の修正

--- src/displayProgress.ts
+++ src/displayProgress.ts
@@ -19,12 +19,12 @@ function padSpace(str:string, num:number=9){
 /**
  * @fn
  * 進度を示す関数を返す関数。
- * 項目を保存するためのitems[]、出力した数を保存するためのnumberOfOutputを保持
+ * 項目を保存するためのitems[]、出力した数を保存するためのnumberOfProgressを保持
  * @brief 進度を示す関数を返す関数
  */
 export function makeDisplayProgress(){
     // 出力している#の数を保存する変数
-    let numberOfOutput:number = 0;
+    let numberOfProgress:number = 0;
     // 出力から得た項目(Parser,MSM等)が「コマンドを実行してから初めて得た項目なのか」を判定するために使うリスト
     // コマンドの実行ごとに空リストから始まり、Parser,MSM,Analyzer等の取得した項目をpushする
     let items:string[] = [];
@@ -34,14 +34,14 @@ export function makeDisplayProgress(){
      * プログラムの出力を受け取り、その進度を示す関数
      * @param (channel) 出力するためのチャンネル
      * @param (line) プログラムが出力した1行
-     * @param (numberOfArticleLines) mizarのarticle部の行数
-     * @param (numberOfBeginLine) mizarのarticle部が始まる前の行番号
+     * @param (numberOfArticleLines) mizarの記述部の行数
+     * @param (numberOfEnvironmentalLines) mizarの環境部の行数
      */
     function _displayProgress(
         channel:vscode.OutputChannel,
         line:string,
         numberOfArticleLines:number,
-        numberOfBeginLine:number
+        numberOfEnvironmentalLines:number
     ):number
     {
         // REVIEW:正規表現が正しいか確認
@@ -49,13 +49,13 @@ export function makeDisplayProgress(){
         // 「Parser」や「3482」にあたる部分をグループ化している
         let matched = line.match(/^(\w+) +\[ *(\d+) *\**\d*\].*$/);
         if(matched === null){
-            return numberOfOutput;
+            return numberOfProgress;
         }
         // 実行して初めて得た項目であった時の前処理
         if (items.indexOf(matched[1],0) === -1){
             if(items.length !== 0){
                 // 直前の項目の#が50未満であれば、足りない分を#で補完
-                for (let i = 0; i < 50 - numberOfOutput; i++){
+                for (let i = 0; i < 50 - numberOfProgress; i++){
                     channel.append("#");
                 }
                 channel.appendLine("");
@@ -65,20 +65,20 @@ export function makeDisplayProgress(){
             channel.append(item +':');
             // 前処理を終えた項目として、itemsにpush
             items.push(matched[1]);
-            numberOfOutput = 0;
+            numberOfProgress = 0;
         }
         // 現在の進度と、出力のバーの差を計算する
-        let progress = (Number(matched[2]) - numberOfBeginLine) 
-                        / numberOfArticleLines * 50 - numberOfOutput;
+        let progressDiff = (Number(matched[2]) - numberOfEnvironmentalLines) 
+                        / numberOfArticleLines * 50 - numberOfProgress;
         // 「現在の進度-出力している進度」の差を埋める出力処理
-        for (let i = 0; i < Math.floor(progress); i++){
-            if (numberOfOutput >= 50){
+        for (let i = 0; i < Math.floor(progressDiff); i++){
+            if (numberOfProgress >= 50){
                 break;
             }
             channel.append("#");
-            numberOfOutput += 1;
+            numberOfProgress += 1;
         }
-        return numberOfOutput;
+        return numberOfProgress;
     }
     return _displayProgress;
 }
