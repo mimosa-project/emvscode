7e12a7f5554d067a6dad96084dae8aade46fe77a
fpsbpkm <taisei4510471a@yahoo.co.jp>
2019-11-25 15:00:41 +0900

    verifier以外のコマンドの処理を追加と後処理関数の追加、変数名の修正。

--- src/extension.ts
+++ src/extension.ts
@@ -1,17 +1,14 @@
 import * as vscode from 'vscode';
 import * as path from 'path';
-import { mizar_verify, mizfiles } from './mizarFunctions';
+import { mizar_verify, mizfiles, postProcessing } from './mizarFunctions';
 import { makeQueryFunction } from './mizarMessages';
-import { displayErrorLinks } from './displayErrors';
 
 export const queryMizarMsg = makeQueryFunction();
 
 export function activate(context: vscode.ExtensionContext) {
-
-    //verifierの実行結果を出力するチャンネル
+    // verifierの実行結果を出力するチャンネル
     var channel = vscode.window.createOutputChannel('output');
     channel.show();
-
     let diagnosticCollection = vscode.languages.createDiagnosticCollection('mizar');
     let diagnostics:vscode.Diagnostic[] = [];
 
@@ -19,58 +16,151 @@ export function activate(context: vscode.ExtensionContext) {
         vscode.window.showInformationMessage('Hello World!');
     });
 
-    //mizar-verifyの処理
+    // mizar-verifyの処理
     let disposable2 = vscode.commands.registerCommand('mizar-verify', async () => {
-        
-        //アクティブなエディタがなければエラーを示して終了
+        // アクティブなエディタがなければエラーを示して終了
         if (vscode.window.activeTextEditor === undefined){
             vscode.window.showErrorMessage('Not currently in .miz file!!');
             return;
         }
-
         diagnostics = [];
         diagnosticCollection.clear();
-
-        //アクティブなファイルのパスを取得
+        // アクティブなファイルのパスを取得
         let uri = vscode.window.activeTextEditor.document.uri;
         let fileName =vscode.window.activeTextEditor.document.fileName;
-
-        //makeenvとverifierの実行
+        // makeenvとverifierの実行
         let result = await mizar_verify(channel,fileName);
-
-        //makeenv,verifierの結果でエラーがあれば、エラー表示関数を呼び出す
-        switch(result){
-            case "success": break;
-            case "makeenv error": 
-            case "verifier error": 
-                displayErrorLinks(channel,fileName,uri,diagnostics,diagnosticCollection);
-        }
+        // makeenv,verifierの結果でエラーがあれば、エラー表示関数を呼び出す処理
+        postProcessing(channel,fileName,uri,result,diagnostics,diagnosticCollection);
     });
-
-    //mizar-verify2の実行
+ 
+    // mizar-verify2の実行
     let disposable3 = vscode.commands.registerCommand('mizar-verify2', async () => {
         //アクティブなエディタがなければエラーを示して終了
         if (vscode.window.activeTextEditor === undefined){
             vscode.window.showErrorMessage('Not currently in .miz file!!');
             return;
         }
-
         diagnostics = [];
         diagnosticCollection.clear();
-
-        //アクティブなファイルのパスを取得
+        // アクティブなファイルのパスを取得
         let fileName = vscode.window.activeTextEditor.document.fileName;
-        //errflag.exeの絶対パスを格納
+        // errflag.exeの絶対パスを格納
         let errFlag = path.join(String(mizfiles),"errflag");
-        //makeenvとverifierの実行
+        // makeenvとverifierの実行
         await mizar_verify(channel,fileName);
-        //errflag.exeの実行(テキスト内にエラーのフラグを書き込む)
+        // errflag.exeの実行(テキスト内にエラーのフラグを書き込む)
         require('child_process').spawn(errFlag,[fileName]);
+    });
+
+    let disposable4 = vscode.commands.registerCommand('mizar-irrths', async () => {
+        if (vscode.window.activeTextEditor === undefined){
+            vscode.window.showErrorMessage('Not currently in .miz file!!');
+            return;
+        }
+        let fileName = vscode.window.activeTextEditor.document.fileName;
+        let uri = vscode.window.activeTextEditor.document.uri;
+        diagnostics = [];
+        diagnosticCollection.clear();
+        let result = await mizar_verify(channel,fileName,"irrths");
+        // makeenv,irrthsの結果でエラーがあれば、エラー表示関数を呼び出す処理
+        postProcessing(channel,fileName,uri,result,diagnostics,diagnosticCollection);
+    });
+
+    let disposable5 = vscode.commands.registerCommand('mizar-relinfer', async () => {
+        if (vscode.window.activeTextEditor === undefined){
+            vscode.window.showErrorMessage('Not currently in .miz file!!');
+            return;
+        }
+        let fileName = vscode.window.activeTextEditor.document.fileName;
+        let uri = vscode.window.activeTextEditor.document.uri;
+        diagnostics = [];
+        diagnosticCollection.clear();
+        let result = await mizar_verify(channel,fileName,"relinfer");
+        // makeenv,relinferの結果でエラーがあれば、エラー表示関数を呼び出す処理
+        postProcessing(channel,fileName,uri,result,diagnostics,diagnosticCollection);
+    });
+
+    let disposable6 = vscode.commands.registerCommand('mizar-trivdemo', async () => {
+        if (vscode.window.activeTextEditor === undefined){
+            vscode.window.showErrorMessage('Not currently in .miz file!!');
+            return;
+        }
+        let fileName = vscode.window.activeTextEditor.document.fileName;
+        let uri = vscode.window.activeTextEditor.document.uri;
+        diagnostics = [];
+        diagnosticCollection.clear();
+        let result = await mizar_verify(channel,fileName,"trivdemo");
+        // makeenv,trivdemoの結果でエラーがあれば、エラー表示関数を呼び出す処理
+        postProcessing(channel,fileName,uri,result,diagnostics,diagnosticCollection);
+    });
+
+    let disposable7 = vscode.commands.registerCommand('mizar-reliters', async () => {
+        if (vscode.window.activeTextEditor === undefined){
+            vscode.window.showErrorMessage('Not currently in .miz file!!');
+            return;
+        }
+        let fileName = vscode.window.activeTextEditor.document.fileName;
+        let uri = vscode.window.activeTextEditor.document.uri;
+        diagnostics = [];
+        diagnosticCollection.clear();
+        let result = await mizar_verify(channel,fileName,"reliters");
+        // makeenv,relitersの結果でエラーがあれば、エラー表示関数を呼び出す処理
+        postProcessing(channel,fileName,uri,result,diagnostics,diagnosticCollection);
+    });
+
+    let disposable8 = vscode.commands.registerCommand('mizar-relprem', async () => {
+        if (vscode.window.activeTextEditor === undefined){
+            vscode.window.showErrorMessage('Not currently in .miz file!!');
+            return;
+        }
+        let fileName = vscode.window.activeTextEditor.document.fileName;
+        let uri = vscode.window.activeTextEditor.document.uri;
+        diagnostics = [];
+        diagnosticCollection.clear();
+        let result = await mizar_verify(channel,fileName,"relprem");
+        // makeenv,relpremの結果でエラーがあれば、エラー表示関数を呼び出す処理
+        postProcessing(channel,fileName,uri,result,diagnostics,diagnosticCollection);
+    });
+
+    let disposable9 = vscode.commands.registerCommand('mizar-irrvoc', async () => {
+        if (vscode.window.activeTextEditor === undefined){
+            vscode.window.showErrorMessage('Not currently in .miz file!!');
+            return;
+        }
+        let fileName = vscode.window.activeTextEditor.document.fileName;
+        let uri = vscode.window.activeTextEditor.document.uri;
+        diagnostics = [];
+        diagnosticCollection.clear();
+        let result = await mizar_verify(channel,fileName,"irrvoc");
+        // makeenv,irrvocの結果でエラーがあれば、エラー表示関数を呼び出す処理
+        postProcessing(channel,fileName,uri,result,diagnostics,diagnosticCollection);
+    });
+
+    let disposable10 = vscode.commands.registerCommand('mizar-inacc', async () => {
+        if (vscode.window.activeTextEditor === undefined){
+            vscode.window.showErrorMessage('Not currently in .miz file!!');
+            return;
+        }
+        let fileName = vscode.window.activeTextEditor.document.fileName;
+        let uri = vscode.window.activeTextEditor.document.uri;
+        diagnostics = [];
+        diagnosticCollection.clear();
+        let result = await mizar_verify(channel,fileName,"inacc");
+        // makeenv,inaccの結果でエラーがあれば、エラー表示関数を呼び出す処理
+        postProcessing(channel,fileName,uri,result,diagnostics,diagnosticCollection);
     });
 
     context.subscriptions.push(disposable1);
     context.subscriptions.push(disposable2);
     context.subscriptions.push(disposable3);
+    context.subscriptions.push(disposable4);
+    context.subscriptions.push(disposable5);
+    context.subscriptions.push(disposable6);    
+    context.subscriptions.push(disposable7);
+    context.subscriptions.push(disposable8);
+    context.subscriptions.push(disposable9);
+    context.subscriptions.push(disposable10);
 }
 
 // this method is called when your extension is deactivated
--- src/mizarFunctions.ts
+++ src/mizarFunctions.ts
@@ -2,6 +2,8 @@ import * as vscode from 'vscode';
 import * as path from 'path';
 import { makeDisplayProgress,MAX_OUTPUT } from './displayProgress';
 import { countLines } from './countLines';
+import { displayErrorLinks } from './displayErrors';
+import { resolve } from 'dns';
 
 const carrier = require('carrier');
 const Makeenv = "makeenv";
@@ -10,58 +12,57 @@ export const mizfiles = process.env.MIZFILES;
 
 /**
  * @fn
- * fileNameで与えられたファイルに対して、makeenv,verifierを実行し、"success","makeenv error","verifier error"
+ * fileNameで与えられたファイルに対して、makeenv,commandを実行し、"success","makeenv error","command error"
  * のいずれかを返す関数
- * @brief makeenv,verifierを実行する関数
+ * @brief makeenv,commandを実行する関数
  * @param (channel) 結果を出力するチャンネル
- * @param (fileName) makeenv,verifierが実行する対象のファイル名
- * @param (util) 実行するコマンド、デフォルトで"verifier"となっており、現時点では"verifier"以外使っていない
- * @return コマンドの実行結果を,"success","makeenv error", "verifier error"で返す
+ * @param (fileName) makeenv,commandが実行する対象のファイル名
+ * @param (command) 実行するコマンド、デフォルトでは"verifier"となっている
+ * @return コマンドの実行結果を,"success","makeenv error", "command error"で返す
  */
 export async function mizar_verify(
     channel:vscode.OutputChannel, 
     fileName:string, 
-    util:string="verifier"
-)
+    command:string="verifier"
+):Promise<string>
 {
-    //コマンドを絶対パスにしている
-    util = path.join(String(mizfiles) ,util);
+    // コマンドを絶対パスにしている
+    command = path.join(String(mizfiles) ,command);
     let makeenv = path.join(String(mizfiles),Makeenv);
-    //拡張子を確認し、mizarファイルでなければエラーを示して終了
+    // 拡張子を確認し、mizarファイルでなければエラーを示して終了
     if (path.extname(fileName) !== '.miz'){
         vscode.window.showErrorMessage('Not currently in .miz file!!');
-        return;
+        return "file error";
     }
     channel.clear();
     channel.show();
     const displayProgress = makeDisplayProgress();
-    //makeenvの実行
+    // makeenvの実行
     let makeenvProcess = require('child_process').spawn(makeenv,[fileName]);
     let isMakeenvSuccess = true;
     let isVerifierSuccess = true;
     carrier.carry(makeenvProcess.stdout, (line:string) => {
         channel.appendLine(line);
-
         if (line.indexOf('*') !== -1){
             isMakeenvSuccess = false;
         }
     });
-    //非同期処理から実行結果を得るため、Promiseを利用している
-    let result = new Promise((resolve) => {
+    // 非同期処理から実行結果を得るため、Promiseを利用している
+    let result:Promise<string> = new Promise((resolve) => {
         makeenvProcess.on('close', () => {
             if(!isMakeenvSuccess){
                 resolve('makeenv error');
                 return;
             }
-            channel.appendLine("Running " + path.basename(util) 
+            channel.appendLine("Running " + path.basename(command) 
                                 + " on " + fileName + '\n');
             channel.appendLine("   Start |------------------------------------------------->| End");
             let [numberOfEnvironmentalLines,
                 numberOfArticleLines] = countLines(fileName);
             let numberOfProgress:number = 0;
             let numberOfErrors:number = 0;
-            let verifierProcess = require('child_process').spawn(util,[fileName]);
-            carrier.carry(verifierProcess.stdout, (line:string) => {
+            let commandProcess = require('child_process').spawn(command,[fileName]);
+            carrier.carry(commandProcess.stdout, (line:string) => {
                 // lineを渡してプログレスバーを表示する関数を呼び出す
                 [numberOfProgress,numberOfErrors] = displayProgress(channel,line,
                     numberOfArticleLines,numberOfEnvironmentalLines);
@@ -69,7 +70,7 @@ export async function mizar_verify(
                     isVerifierSuccess = false;
                 }
             }, null, /\r/);
-            verifierProcess.on('close',() => {
+            commandProcess.on('close',() => {
                 // 最後の項目のプログレスバーがMAX_OUTPUT未満であれば、足りない分を補完
                 let appendChunk = "#".repeat(MAX_OUTPUT-numberOfProgress);
                 channel.append(appendChunk);
@@ -79,7 +80,7 @@ export async function mizar_verify(
                 }
                 channel.appendLine("\n\nEnd.\n");
                 if (!isVerifierSuccess){
-                    resolve('verifier error');
+                    resolve('command error');
                 }
                 else{
                     resolve('success');
@@ -88,4 +89,21 @@ export async function mizar_verify(
         });
     });
     return await result;
+}
+
+export function postProcessing(
+    channel:vscode.OutputChannel,
+    fileName:string,
+    uri:vscode.Uri,
+    result:string,
+    diagnostics:vscode.Diagnostic[],
+    diagnosticCollection:vscode.DiagnosticCollection
+)
+{
+    switch(result){
+        case "success": break;
+        case "makeenv error":
+        case "command error":
+            displayErrorLinks(channel,fileName,uri,diagnostics,diagnosticCollection);
+    }
 }
