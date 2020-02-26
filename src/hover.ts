e9e7c2fe5cd0b159f433bfc5f209f2c086b2a407
fpsbpkm <taisei4510471a@yahoo.co.jp>
2020-02-26 16:35:37 +0900

    コードの整形

--- src/hover.ts
+++ src/hover.ts
@@ -1,80 +1,85 @@
 import * as vscode from 'vscode';
 import * as path from 'path';
 import * as fs from 'fs';
-
-function indexOfNumber(allText:string,search:string,number:number){
+// n番目のsearchStringの位置を返す関数
+function indexOfNumber(text:string,searchString:string,n:number){
     var index = -1;
-    for(let i = 0; i < number; i++){
-        index = allText.indexOf(search,index+1);
+    for(let i = 0; i < n; i++){
+        index = text.indexOf(searchString,index+1);
     }
     return index;
 }
 
-function returnExternalFileHover(word:string, wordRange:vscode.Range, fileName:string, numberOfreference:string){
+function returnHover(document:vscode.TextDocument, wordRange:vscode.Range, position:vscode.Position){
     try{
-        let start = 0,end = 0;
+        let startIndex = 0,endIndex = 0;
         // TODO:非同期へ修正
-        let textOfReference = "" + fs.readFileSync(fileName);
-
-        // 場合に応じて、それぞれ参照箇所を抽出する
-
+        let textOfReference = "" + fs.readFileSync(document.fileName);
+        let word = document.getText(wordRange);
         // definitionを参照する場合
-        if(word.indexOf('def') >= 0){
-            start = indexOfNumber(textOfReference, 'definition', Number(numberOfreference.slice('def '.length))+1);
-            end = start + textOfReference.slice(start).search(/\nend;/g) + '\nend;'.length;
-        }
-        // schemeを参照する場合
-        else if(word.indexOf('sch') >= 0){
-            start = indexOfNumber(textOfReference, 'scheme', Number(numberOfreference.slice('sch '.length))+1);
-            end = start + textOfReference.slice(start).search(/\nend;/g) + '\nend;'.length;
-        }
-        // theoremを参照する場合
-        else{
-            start = indexOfNumber(textOfReference, 'theorem', Number(numberOfreference)+1);
-            end = start + textOfReference.slice(start).search(/(\nproof|;)/g) + '\n'.length;
-        }
-
-        let markedString = {language:"Mizar", value: textOfReference.slice(start,end)};
-        let hover = new vscode.Hover(markedString, wordRange);
-        return hover;
-    }catch(e){
-        console.log(e);
-        vscode.window.showErrorMessage("Can't open " + fileName + '!!!');
-        return;
-    }
-}
-
-function returnInternalFileHover(word:string, wordRange:vscode.Range, fileName:string, position:vscode.Position){
-    let start = 0,end = 0;
-    // TODO:非同期へ修正
-    let textOfReference = "" + fs.readFileSync(fileName);
-    try{
-        // definitionを参照する場合
-        if(word.indexOf('Def') >= 0){
+        if(word.indexOf('Def') > -1){
             // TODO:+1の修正、見直し
-            start = indexOfNumber(textOfReference, 'definition', Number(word.slice('Def'.length))+1);
-            end = start + textOfReference.slice(start).search(/\nend;/g) + '\nend;'.length;
+            startIndex = indexOfNumber(textOfReference, 'definition', Number(word.slice('Def'.length))+1);
+            endIndex = startIndex + textOfReference.slice(startIndex).search(/\nend;/g) + '\nend;'.length;
         }
         // theoremを参照する場合
-        else if(word.indexOf('Th') >= 0){
-            start = indexOfNumber(textOfReference, 'theorem', Number(word.slice('Th'.length))+1);
-            end = start + textOfReference.slice(start).search(/(\nproof|;)/g) + '\n'.length;
+        else if(word.indexOf('Th') > -1){
+            startIndex = indexOfNumber(textOfReference, 'theorem', Number(word.slice('Th'.length))+1);
+            endIndex = startIndex + textOfReference.slice(startIndex).search(/(\nproof|;)/g) + '\n'.length;
         }
         // ラベルを参照する場合
         else{
             // ホバー中の現在の行までの文字数を取得
             let number = indexOfNumber(textOfReference,'\n', position.line);
             // 直前のラベルの定義元の位置を取得
-            start = textOfReference.lastIndexOf(word+':', number);
-            end = start + textOfReference.slice(start).indexOf('\n');
+            startIndex = textOfReference.lastIndexOf(word.replace(/( |,)/, '')+':', number);
+            endIndex = startIndex + textOfReference.slice(startIndex).indexOf('\n');
             
         }
 
-        let markedString = {language:"Mizar", value: textOfReference.slice(start,end)};
+        let markedString = {language:"Mizar", value: textOfReference.slice(startIndex,endIndex)};
         let hover = new vscode.Hover(markedString, wordRange);
         return hover;
 
     }catch(e){
+        return;
+    }
+}
+
+function returnMMLHover(document:vscode.TextDocument,wordRange:vscode.Range):vscode.ProviderResult<vscode.Hover>{
+    if(process.env.MIZFILES === undefined){
+        vscode.window.showErrorMessage('error!');
+        return;
+    }
+    try{
+        let mmlPath = path.join(process.env.MIZFILES,'mml');
+        let word = document.getText(wordRange);
+        let [fileName, referenceWord] = word.split(':');
+        fileName = path.join(mmlPath,fileName.toLowerCase() + '.miz');
+        let startIndex = 0,endIndex = 0;
+        // TODO:非同期へ修正
+        let referenceText = "" + fs.readFileSync(fileName)
+            // definitionを参照する場合
+            if(referenceWord.indexOf('def') > -1){
+                startIndex = indexOfNumber(referenceText, 'definition', Number(referenceWord.slice('def '.length))+1);
+                endIndex = startIndex + referenceText.slice(startIndex).search(/\nend;/g) + '\nend;'.length;
+            }
+            // schemeを参照する場合
+            else if(referenceWord.indexOf('sch') > -1){
+                startIndex = indexOfNumber(referenceText, 'scheme', Number(referenceWord.slice('sch '.length))+1);
+                endIndex = startIndex + referenceText.slice(startIndex).search(/\nend;/g) + '\nend;'.length;
+            }
+            // theoremを参照する場合
+            else{
+                startIndex = indexOfNumber(referenceText, 'theorem', Number(referenceWord)+1);
+                endIndex = startIndex + referenceText.slice(startIndex).search(/(\nproof|;)/g) + '\n'.length;
+            }
+            let markedString = {language:"Mizar", value: referenceText.slice(startIndex,endIndex)};
+            let hover = new vscode.Hover(markedString, wordRange);
+            return hover;
+    }catch(e){
+        console.log(e);
+        vscode.window.showErrorMessage("Can't open file!");
         return;
     }
 }
@@ -86,35 +91,22 @@ export class HoverProvider implements vscode.HoverProvider{
         position: vscode.Position,
         token:vscode.CancellationToken
     ):vscode.ProviderResult<vscode.Hover>{
-
-        if(process.env.MIZFILES === undefined){
-            vscode.window.showErrorMessage('error!');
-            return;
-        }
-
         // ホバーの範囲を正規表現により取得
-        let wordRange = document.getWordRangeAtPosition(position,/(\w+:def \d+|\w+:sch \d+|\w+:\d+)/);
-        let word = document.getText(wordRange);
+        let wordRange;
 
-        // 現在開いているファイルの定義、定理を参照する場合
-        if(wordRange === undefined){
-            let fileName = document.fileName;
-            let wordRange = document.getWordRangeAtPosition(position,/(Def\d+|Th\d+|A\d+)/);
-            let word = document.getText(wordRange);
-            if (wordRange === undefined){
-                return;
-            }
-            let hover = returnInternalFileHover(word, wordRange, fileName, position);
-            return hover;
+        // 自身のファイル内の定義、定理、ラベルを参照する場合
+        // 「Def10」「Th1」「 A1」「,A2」等を正規表現で取得する
+        if (wordRange = document.getWordRangeAtPosition(position,/(Def\d+|Th\d+|( |,)A\d+)/)){
+            return returnHover(document, wordRange, position);
         }
         // 外部ファイル（MML）の定義、定理、スキームを参照する場合
+        // 「FUNCT_2:def 1」「FINSUB_1:13」「XBOOLE_0:sch 1」等を正規表現で取得する
+        else if (wordRange = document.getWordRangeAtPosition(position,/(\w+:def \d+|\w+:\d+|\w+:sch \d+)/)){
+            return returnMMLHover(document,wordRange);
+        }
+        // ホバー対象のキーワードでない場合
         else{
-            let mmlPath = path.join(process.env.MIZFILES,'mml');
-            let [fileName, numberOfreference] = word.split(':');
-            fileName = path.join(mmlPath,fileName.toLowerCase() + '.miz');
-            // 外部ファイルから適切なホバー情報を取得する
-            let hover = returnExternalFileHover(word, wordRange, fileName, numberOfreference);
-            return hover;
+            return;
         }
     }
 }
