fae62efa2857624662b81db34618e32814c35281
fpsbpkm <a089vgu@yamaguchi-u.ac.jp>
2021-10-14 23:43:17 +0900

    コメントの修正

--- src/countLines.ts
+++ src/countLines.ts
@@ -16,11 +16,11 @@ export function countLines(fileName:string):number[]{
     let isArticleArea = false;
     for (let line of lines){
         // begin以降から記述部が始まるためisArticleAreaにtrueを設定
-        // NOTE:コメント内に「begin」が存在する場合正しく動作しない
+        // NOTE:コメント内に「begin」が存在しても問題が無いように，正規表現を利用
         // NG例：
         // 「:: begin」
         // 「:: aaa begin bbb」
-        if(! /^(::|:::)\s*\w*\s*begin\s*\w*/.test(line)){
+        if(!/^(::|:::)\s*\w*\s*begin\s*\w*/.test(line)){
             isArticleArea = true;
         }
         // 記述部の行数のカウント
